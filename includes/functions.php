fe20ddf9dca877a241f012f49c3dc2945fa1f597
Katherine Erickson <katherine.erickson@gmail.com>
2015-04-17 11:16:28 -0400

    bug fix

--- includes/functions.php
+++ includes/functions.php
@@ -5,37 +5,39 @@
 	/*  reconfigure a date.
 	    accepts date in format: yearmonthdate.
 	    returns date in format: month/date/year. */
-	    
-	function reconfigure_date($date){
-		if ($date) {
-			$times = explode('-', $date);
-			return $times[1] . "/" . $times[2] . "/" . $times[0];
-		} else {
-			return "";
-		}
-	}
-	
-	
-	
+
+  if (!function_exists('reconfigure_date')) {
+    function reconfigure_date($date){
+      if ($date) {
+        $times = explode('-', $date);
+        return $times[1] . "/" . $times[2] . "/" . $times[0];
+      } else {
+        return "";
+      }
+    }
+  }
+
+
+
 	/*  generate a wormbase link from a strain name.
 	    returns wormbase link as string. */
-	    
+
 	function generate_wormbase($strain) {
-	    return preg_replace('/strain_fill/', $strain, 'http://wormbase.org/species/c_elegans/strain/strain_fill#01--9');	
+	    return preg_replace('/strain_fill/', $strain, 'http://wormbase.org/species/c_elegans/strain/strain_fill#01--9');
 	}
-	
-	
-	
+
+
+
 	/*  generate genotype using pieces in the database.
-	    this function takes the genotype and transgene_id fields from 
+	    this function takes the genotype and transgene_id fields from
 	        the 'strains' table.
-	    in some cases the genotype field represents the complete genotype; in 
+	    in some cases the genotype field represents the complete genotype; in
 	        these cases, the unaltered genotype is returned.
 	    if the transgene_id field is populated, then the genotype serves as
 	        a genotype template, in which case the other pieces from other
 	        tables are assembled.
 	    returns the genotype as a string. */
-	
+
 	function generate_genotype($genotype, $transgene_id) {
 		// retrieve and define the genotype template
 		$query = "SELECT genotype FROM genotype WHERE id = $genotype";
@@ -47,11 +49,11 @@
 		while ($row = mysql_fetch_assoc($result)) {
 			$genotype = $row['genotype'];
 		}
-		
+
 		// if there is a transgene id provided
 		if ($transgene_id != NULL) {
 			// retrieve and define the transgene template
-			$query = "SELECT transgene.name AS transgene_name, 
+			$query = "SELECT transgene.name AS transgene_name,
 					vector.gene, vector.sequence,
 					vector.promotor, vector.threePrimeUTR,
 					vector_template.genotype AS vector_template_genotype
@@ -75,34 +77,34 @@
 				$promotor = $row['promotor'];
 				$threePrimeUTR = $row['threePrimeUTR'];
 			}
-			
+
 			// if the gene is null, use the sequence
 			if ($gene == NULL) {
 				$gene = $sequence;
 			}
-			
+
 			// replace holes in vector (gene, promotor, and/or threePrimeUTR)
 			$vector_genotype = str_replace('gene', $gene, $vector_genotype);
 			$vector_genotype = str_replace('promotor', $promotor, $vector_genotype);
 			$vector_genotype = str_replace('threePrimeUTR', $threePrimeUTR, $vector_genotype);
-			
+
 			// replace holes in genotype
 			$genotype = str_replace('vector_genotype', $vector_genotype, $genotype);
-			$genotype = str_replace('transgene_name', $transgene_name, $genotype);			
+			$genotype = str_replace('transgene_name', $transgene_name, $genotype);
 		}
-		
+
 		// return the genotype
 		return $genotype;
 	}
-	
-	
-	
+
+
+
 	/*  display contents of a freezer rack.
 	    returns nothing. */
 
 	function rack_contents($rack_id, $slots_horizontal_count, $slots_vertical_count) {
 		// get box information for all boxes in the rack //
-		$query = "SELECT storage_box.box_name, storage_box.id AS box_id, 
+		$query = "SELECT storage_box.box_name, storage_box.id AS box_id,
 				storage_box.old_location, authors.author
 			FROM storage_box
 			LEFT JOIN authors
@@ -116,7 +118,7 @@
 			echo 'Could not run query: ' . mysql_error();
 			exit;
 		}
-		
+
 		if (mysql_num_rows($result) == 0) {
 			echo "<td class='emptyEmphasis'>No Record</td>";
 		} else {
@@ -126,7 +128,7 @@
 				$box_id = $row['box_id'];
 				$old_location = $row['old_location'];
 				$author = $row['author'];
-				
+
 				if ($old_location != NULL) {
 					if ($box_name != NULL && $author != NULL) {
 						echo "<td class='wholeLink'><a href='/storage/tube_view.php?box_id=$box_id'>$box_name
@@ -168,7 +170,7 @@
 			}
 		}
 	}
-	
+
 	// HAVEN'T IMPLEMENTED THE FOLLOWING FUNCTIONS //
 	// sets a word limit on a string (for display purposes)
 	function string_limit_words($string, $word_limit) {
